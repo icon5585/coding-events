f46620843b92b90bceba60d935832cb21effb129
Hank DeDona <icon5585@gmail.com>
2020-03-02 19:20:06 -0600

    Finished exercises 17.4

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,41 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class EventCategory {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Size(min = 3, message = "Name must be at least 3 characters long")
+	private String name;
+
+	public EventCategory() {
+	}
+
+	public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+    public String toString() {
+        return name;
+    }
+}
